5c484e3c58f62eccc30f6356c20ce2274321cc73
Flintex <flintex1@gmail.com>
2024-03-17 17:56:17 +0700

    update my_new_project code

--- my_new_project/main.cpp
+++ my_new_project/main.cpp
@@ -6,15 +6,16 @@ int main()
     std::cout << "TODO" << std::endl;
     // TODO Do smth
 
-    std::string inputName{}; //Объявляем текстовую переменную Имя
+    std::string inputName; //Объявляем текстовую переменную Имя
     std::cout << "Введите ваше имя" << std::endl;
-    std::cin >> inputName; //Обновляем текстовую переменную из ввода
+    std::cin >> inputName; //Инициализируем текстовую переменную из ввода
 
     int inputAge; //Объявляем цифровую переменную Возраст
     std::cout << "Введите ваш возраст" << std::endl;
-    std::cin >> inputAge; //Обновляем цифровую переменную из ввода
+    std::cin >> inputAge; //Инициализируем цифровую переменную из ввода
 
     std::cout << "Вас зовут: " << inputName << ". Ваш возрас: " << inputAge << std::endl; //Выводим переменные
+    std::cout << "inputName is " << sizeof(inputName) << std::endl;
 
     return 0;
 }
